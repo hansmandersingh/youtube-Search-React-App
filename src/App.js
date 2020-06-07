b606ddc8e466c319dd6b1bb689175160a08c130e
Hans <hansmandersingh1998@gmail.com>
2020-06-06 21:00:11 -0500

    Prints searched results

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./Header";
-import Video from './Video';
+import Video from "./Video";
 
 class App extends React.Component {
   state = {
@@ -20,23 +20,25 @@ class App extends React.Component {
   onSearch = (e, query) => {
     e.preventDefault();
 
-    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&key=AIzaSyAfXif5ow-aL4o3qmngxCeezF9hWCvtV14&q=${query}`)
-      .then(data => data.json())
-      .then(json => {
-        console.log(json)
-      })
-  }
+    fetch(
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&key=AIzaSyAfXif5ow-aL4o3qmngxCeezF9hWCvtV14&q=${query}`
+    )
+      .then((data) => data.json())
+      .then((json) => {
+        this.setState({ latest: json.items });
+      });
+  };
 
   render = () => {
     return (
       <>
-        <Header onSearch={this.onSearch}/>
+        <Header onSearch={this.onSearch} />
         <div className="titleList">
           <div className="title">
             {this.state.latest.length > 0 && <h1>Videos</h1>}
             <div className="titles-wrapper">
-              {this.state.latest.map(video => (
-                <Video key={video.id.videoId} video={video}/>
+              {this.state.latest.map((video) => (
+                <Video key={video.id.videoId} video={video} />
               ))}
             </div>
           </div>
